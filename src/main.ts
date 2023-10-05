deaac74c0ca966c3a641f134facd8f2fa868dbee
Kovács Tibor <tibor.kovacs@quest.com>
2023-10-05 13:24:01 +0200

    build(eslint-import): add import eslint plugin to sort the import declarations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { importProvidersFrom } from "@angular/core";
+import { bootstrapApplication, BrowserModule } from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
+
 import { AppComponent } from "./app/app.component";
 import { APP_ROUTES } from "./app/app-routes";
-import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
-import { provideRouter } from "@angular/router";
 
 bootstrapApplication(AppComponent, {
   providers: [importProvidersFrom(BrowserModule), provideRouter(APP_ROUTES)],
