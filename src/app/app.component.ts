e53b2faec2a85234072d395232514ff7e4a94399
Kovács Tibor <tibor.kovacs@quest.com>
2023-10-07 23:57:42 +0200

    fix(app-component): add router module to imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
+  imports: [RouterModule],
   styleUrls: ["./app.component.scss"],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
